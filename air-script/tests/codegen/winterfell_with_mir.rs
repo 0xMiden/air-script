a08d16ba1b60e7d24ecc3a7c8519cd50013544f5
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2025-04-10 15:55:50 +0200

    add varlen test to the codegen tests and improve the test

--- air-script/tests/codegen/winterfell_with_mir.rs
+++ air-script/tests/codegen/winterfell_with_mir.rs
@@ -35,6 +35,16 @@ fn buses_complex() {
 }
 
 #[test]
+fn buses_varlen_boundary() {
+    let generated_masm = Test::new("tests/buses/buses_varlen_boundary.air".to_string())
+        .transpile(Target::Winterfell, Pipeline::WithMIR)
+        .unwrap();
+
+    let expected = expect_file!["../buses/buses_varlen_boundary.rs"];
+    expected.assert_eq(&generated_masm);
+}
+
+#[test]
 fn periodic_columns() {
     let generated_air = Test::new("tests/periodic_columns/periodic_columns.air".to_string())
         .transpile(Target::Winterfell, Pipeline::WithMIR)
